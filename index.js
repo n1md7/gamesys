02d4a216ae6f55c41a8b6383019c3a27f5e87432
bichiko <bikodua@gmail.com>
2020-03-23 13:38:52 +0200

    Semicolon added :)

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const http = require( 'http' );
-const routes = require( './src/routes' );
 const {
   port,
   hostname,
   fileCacheTTL,
   dataCacheTTL,
 } = require( './config' );
+const http = require( 'http' );
+const routes = require( './src/routes' );
 
 http.createServer( routes.handler )
  .listen( port, hostname, () => {
    console.log( `Server is running at http://${ hostname }:${ port }/` );
-   console.log('Cache values are in seconds.')
+   console.log('Cache values are in seconds.');
    console.log( { fileCacheTTL, dataCacheTTL } );
  } );
